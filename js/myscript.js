f45b793457fd8ab086d339723553330f55dc0f4f
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-03-28 10:51:29 +0530

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,43 @@
+	$(document).ready(function(){
+		
+		$('.delete').click(function(e){
+			
+			e.preventDefault();
+			
+			var d_id = $(this).attr('data-id');
+			var parent = $(this).parent("td").parent("tr");
+			
+			bootbox.dialog({
+			  message: "Are you sure you want to Delete ?",
+			  title: "<i class='glyphicon glyphicon-trash'></i> Delete !",
+			  buttons: {
+				success: {
+				  label: "No",
+				  className: "btn-success",
+				  callback: function() {
+					 $('.bootbox').modal('hide');
+				  }
+				},
+				danger: {
+				  label: "Delete!",
+				  className: "btn-danger",
+				  callback: function() {		  
+					  
+					  $.post('delete.php', { 'delete':d_id })
+					  .done(function(response){
+						  bootbox.alert(response);
+						  parent.fadeOut('slow');
+					  })
+					  .fail(function(){
+						  bootbox.alert('Something Went Wrog ....');
+					  })
+					  					  
+				  }
+				}
+			  }
+			});
+			
+			
+		});
+		
+	});
